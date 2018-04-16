c1f4eef11d05cffa6f3fa09567066e193bd392e2
Villalobos Granados <graedwin@amazon.com>
2018-04-16 15:12:48 -0600

    Update

--- apps/instadraw/models.py
+++ apps/instadraw/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from models import *
+class Post(models.Model):
+    pic=models.ImageField()
 
-# Create your models here.
--- apps/instadraw/views.py
+++ apps/instadraw/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from ..login_and_registration.models import *
 
-# Create your views here.
+
 def index(request):
     return render(request, 'instadraw/index.html')
 def logout(request):
     request.session.clear()
-    return redirect('/')
+    return redirect('/dashboard')
