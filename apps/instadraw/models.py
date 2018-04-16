eb37fffb71bbd2f591ea3f32c476cc35179de2ae
Villalobos Granados <graedwin@amazon.com>
2018-04-16 15:23:00 -0600

    Update test

--- apps/instadraw/models.py
+++ apps/instadraw/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
-from models import *
+from ..login_and_registration.models import *
+
 class Post(models.Model):
     pic=models.ImageField()
-
+    posted_by=models.ForeignKey(Uer,related_name=posts)
+    liked_by=models.ManyToManyField(User,related_name='likes')
