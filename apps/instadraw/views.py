6716f39a6688a440e688e661dc55ae004d50e581
Villalobos Granados <graedwin@amazon.com>
2018-04-18 16:54:32 -0600

    Updated styles and registration

--- apps/instadraw/views.py
+++ apps/instadraw/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render,redirect
 from ..login_and_registration.models import *
+from django.contrib import messages
 from models import *
 
 
 def index(request):
-    return render(request,'instadraw/index.html',{'posts':Post.objects.all()})
+    if not 'user_id' in request.session:
+        return render(request, 'login_and_registration_app/index.html')
+    else:
+        return render(request,'instadraw/index.html',{'posts':Post.objects.all()})
 def logout(request):
     request.session.clear()
     return redirect('/welcome')
 def new_post(request):
     return render(request,'instadraw/create_post.html')
 def save(request):
-    Post.objects.create(pic=request.POST['image'],posted_by=User.objects.get(id=request.session['user_id']),description=request.POST['description'])
-    return redirect('/instadraw')
+    errors= Post.objects.validate_canvas(request.POST['image'])
+    if errors[0]:
+        Post.objects.create(pic=request.POST['image'],posted_by=User.objects.get(id=request.session['user_id']),description=request.POST['description'])
+        return redirect('/instadraw')
+    else:
+        messages.add_message(request, messages.INFO, errors[1])
+        return redirect('/instadraw/create_post')
+        
