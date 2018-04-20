fdef694f3b0272f5e59fc5b6ec09062c93d610c6
Gerardo <jimgerar@amazon.com>
2018-04-20 15:12:19 -0600

    last update

--- apps/instadraw/urls.py
+++ apps/instadraw/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-<<<<<<< HEAD
-    url(r'logout$', views.logout),
-=======
->>>>>>> changes
     url(r'profile', views.profile),
     url(r'create_post$', views.new_post),
     url(r'create_post/save$', views.save),
